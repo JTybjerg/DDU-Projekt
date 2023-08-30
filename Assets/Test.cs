9d77e131df273e56d3145989dab530d721f706d7
JTybjerg <jeppe2506@gmail.com>
2023-08-30 08:11:51 +0200

    2

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -1,21 +1,8 @@
-
-
-
-
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    public float rotationSpeed = 10f;
-
-    private CharacterController controller;
-    private Vector3 moveDirection;
-
-    void Start()
-    {
-        controller = GetComponent<CharacterController>();
-    }
 
     void Update()
     {
@@ -23,23 +10,16 @@ public class PlayerController : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        // Calculate movement direction based on input and camera orientation
-        Vector3 cameraForward = Camera.main.transform.forward;
-        Vector3 cameraRight = Camera.main.transform.right;
-        cameraForward.y = 0f; // Remove vertical component
-        cameraRight.y = 0f;   // Remove vertical component
-        cameraForward.Normalize();
-        cameraRight.Normalize();
-        moveDirection = cameraForward * verticalInput + cameraRight * horizontalInput;
+        // Calculate movement direction
+        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput);
+
+        // Normalize diagonal movement
+        if (movement.magnitude > 1f)
+        {
+            movement.Normalize();
+        }
 
         // Move the player
-        controller.Move(moveDirection * moveSpeed * Time.deltaTime);
-
-        // Rotate the player to face movement direction
-        if (moveDirection != Vector3.zero)
-        {
-            Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
-        }
+        transform.Translate(movement * moveSpeed * Time.deltaTime);
     }
 }
