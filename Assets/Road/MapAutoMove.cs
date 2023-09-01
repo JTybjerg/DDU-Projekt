1bb9f9fa7f442d79e490082cb28eb252fa883d5b
NikolaiLi <122288063+NikolaiLi@users.noreply.github.com>
2023-09-01 13:59:38 +0200

    hej

--- Assets/Road/MapAutoMove.cs
+++ Assets/Road/MapAutoMove.cs
@@ -1,18 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour
+public class AutoMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public float speed = 5f;
+
+void Update()
+{
+    transform.position = transform.position + (transform.forward * speed * Time.deltaTime);
+}
 }
