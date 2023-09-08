a8d44741d4c723e5782e9488f70f499c5885e0ee
JTybjerg <jeppe2506@gmail.com>
2023-09-08 13:04:41 +0200

    Lanemovement

---
+++ Assets/LaneMovement.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f; // Adjust the movement speed as needed
+    public float startX = -3,5;  // Starting X-coordinate
+    public float endX = 2,5;     // Ending X-coordinate
+
+    private bool movingRight = true;
+
+    void Update()
+    {
+        MoveCharacter();
+    }
+
+    void MoveCharacter()
+    {
+        if (movingRight)
+        {
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+
+            if (transform.position.x >= endX)
+            {
+                movingRight = false;
+            }
+        }
+        else
+        {
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+
+            if (transform.position.x <= startX)
+            {
+                movingRight = true;
+            }
+        }
+    }
+}
